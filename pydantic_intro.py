4c58aed0c62d5f625773bef02feadef7ead60ff4
Shrikant Yalakki <shrikantpy@gmail.com>
2025-07-10 09:18:33 +0530

    Updated codes with BaseModel

--- pydantic_intro.py
+++ pydantic_intro.py
@@ -9,7 +9,7 @@ class Person():
     age:int
     city:str
 
-person = Person(name="Shrkant", age=40, city="Bangalore")
+person = Person(name="Shrikant", age=40, city="Bangalore")
 print(person)
 
 class Person(BaseModel):
@@ -17,10 +17,10 @@ class Person(BaseModel):
     age:int
     city:str
 
-person = Person(name="Shrkant", age=40, city="Bangalore")
+person = Person(name="Shrikant", age=40, city="Bangalore")
 print(person)
 
-#person1 = Person(name="Shrkant", age="forty", city="Bangalore-56")
+#person1 = Person(name="Shrikant", age="forty", city="Bangalore-56")
 #print(person1)
 
 
@@ -38,3 +38,51 @@ class Employee_Opt(BaseModel):
 
 person = Employee_Opt(name="Shrikant", age=40, dept="Engineering")
 print(person)
+
+
+
+# Definition:
+# - Optional[type]: Indicates the field can be None
+# - Default value (= None or = True): Makes the field optional
+# - Required fields must still be provided
+# - Pydantic validates types even for optional fields when values are provided
+
+from typing import List
+# from typing import Dict
+
+class Classroom(BaseModel):
+    room_number: str
+    students: List[str] # List of strings
+    capacity: int
+
+# Create inherited classroom
+classroom = Classroom(room_number='B204', students=["Ramesh", "Suresh", "Govind", "Shambhavi"], capacity=15)
+print(classroom)
+
+try:
+    classroom = Classroom(room_number="B78", students="Suresh", capacity=50)
+except Exception as e:
+    print(e)
+
+# Model with Nested Models
+# Create complex structures with nested models:
+
+class student_score(BaseModel):
+    maths: int
+    physics: int
+    chemestry: int
+    biology: int
+
+class student_detail(BaseModel):
+    name: str
+    roll_num: str
+    score: student_score     # Nested base model
+
+# Creating student with nested details
+
+student = student_detail(name="Ramesh", roll_num="A23", score={"maths": 96, "physics": 92, "chemestry": 93, "biology":95})
+print(student)
+
+# Below one will throw an error
+# student = student_detail(name="Ramesh", roll_num="A23", score={"maths": 96, "physics": "Hello", "chemestry": 93, "biology":95})
+# print(student)
